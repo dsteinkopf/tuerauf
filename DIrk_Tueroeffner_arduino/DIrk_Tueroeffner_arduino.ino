885f9492c0e6bc80080fcb3ac9d219370da4a414
Dirk Steinkopf <dirk@steinkopf.net>
2015-01-11 03:15:33 +0100

    Temperatur mit DHT22 auslesen; Speicher sparen

--- DIrk_Tueroeffner_arduino/DIrk_Tueroeffner_arduino.ino
+++ DIrk_Tueroeffner_arduino/DIrk_Tueroeffner_arduino.ino
@@ -16,12 +16,19 @@ uint8_t *EthernetClient::getRemoteIP(uint8_t remoteIP[])
 }
 I then added the following line (under the virtual void stop(); line)to the EthernetClient.h file:
 uint8_t *getRemoteIP(uint8_t RemoteIP[]);//adds remote ip address
- 
+
+
+***
+
+F("abc") spart Speicher. siehe http://electronics.stackexchange.com/questions/66983/how-to-discover-memory-overflow-errors-in-the-arduino-c-code
  */
+
+#include <stdlib.h>
 
 #include <SPI.h>
 #include <Ethernet.h>
 #include <Timer.h>
+#include <DHT22.h>
 
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFD, 0xEC };
@@ -45,6 +52,15 @@ serverstate mystate = awaiting_fixed_pin;
 const unsigned long timeout_awaiting_dyn_code = 60u*1000u; // Vorsicht long ist offenbar nur 16 bit
 
 int email_pending = 0;
+
+
+const int dht22Pin = 7; // Sensor an Pin D7
+
+float temperaturDHT22 = -999;
+float feuchtigkeit = -999;
+
+DHT22 DHT22_1(dht22Pin); // bilde DHT22-Instanz
+
 
 
 EthernetServer server(ip_port);
@@ -71,23 +87,25 @@ void setup() {
 
   // start the Ethernet connection and the server:
   while (1) {
-    Serial.println("doing DHCP...");
+    Serial.println(F("doing DHCP..."));
     int dhcpok = Ethernet.begin(mac);
     if (dhcpok) {
-      Serial.println("DHCP ok");
+      Serial.println(F("DHCP ok"));
       break;
     }
     else {
-      Serial.println("DHCP failed");
+      Serial.println(F("DHCP failed"));
       delay(5*1000);
     }
   }
   
   server.begin();
-  Serial.print("server is at ");
+  Serial.print(F("server is at "));
   IPAddress myIp = Ethernet.localIP();
   Serial.println(myIp);
   myNet = IPAddress(myIp[0], myIp[1], myIp[2], 0);
+  
+  timer.every(10*1000, dht22);
 }
 
 
@@ -97,7 +115,7 @@ void loop() {
   EthernetClient client = server.available();
   if (client) {
     int charcount = 0;
-    Serial.println("new client");
+    Serial.println(F("new client"));
     // an http request ends with a blank line
     boolean currentLineIsBlank = true;
     while (client.connected()) {
@@ -118,21 +136,21 @@ void loop() {
           String result = processRequest(client, line);
 
           // send a standard http response header
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: text/html");
-          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println(F("HTTP/1.1 200 OK"));
+          client.println(F("Content-Type: text/html"));
+          client.println(F("Connection: close"));  // the connection will be closed after completion of the response
 	  // client.println("Refresh: 5");  // refresh the page automatically every 5 sec
           client.println();
-          client.println("<!DOCTYPE HTML>");
-          client.println("<html>");
-          // client.print("line:");
+          client.println(F("<!DOCTYPE HTML>"));
+          client.println(F("<html>"));
+          // client.print(F("line:");
           // client.print(line);
-          client.println("<br/>");
+          client.println(F("<br/>"));
 
           client.print(result);          
-          client.println("<br/>");       
+          client.println(F("<br/>"));       
 
-          client.println("</html>");
+          client.println(F("</html>"));
 
           break;
         }
@@ -150,33 +168,44 @@ void loop() {
     delay(10);
     // close the connection:
     client.stop();
-    Serial.println("client disconnected");
+    Serial.println(F("client disconnected"));
   }
 }
 
 String processRequest(EthernetClient client, String input)
 {
+  Serial.println(F("processRequest"));
+   
   // check remote ip first - only local net allowed:
   byte rip[] = {0,0,0,0 };
   client.getRemoteIP(rip);
   IPAddress remoteNet(rip[0], rip[1], rip[2], 0); 
   if (myNet != remoteNet) {
-      return "bad client ip";
+      return F("bad client ip");
   }
   
-  Serial.println("processRequest");
+  int pos = input.indexOf(F("gettemp"));
+  if (pos > 0) {
+    return getTemperature();
+  }
+
+  pos = input.indexOf(F("gethum"));
+  if (pos > 0) {
+    return getHumidity();
+  }
+
   switch (mystate) {
     case awaiting_fixed_pin: return checkFixedPin(input); break;
     case awaiting_dyn_code:  return checkDynCode(input); break;
     default: 
       switchToState(awaiting_fixed_pin);
-      return "bad internal state";
+      return F("bad internal state");
   }
 }
 
 String checkFixedPin(String input)
 {
-  Serial.println("checkFixedPin");
+  Serial.println(F("checkFixedPin"));
   int pos = input.indexOf(fixed_pin);
   
   if (pos > 0) {
@@ -198,14 +227,14 @@ String checkFixedPin(String input)
     }
     
     switchToState(awaiting_fixed_pin);
-    return "bad fixed_pin";
+    return F("bad fixed_pin");
   }
 }
 
 String sendNewDynCode()
 {
-  Serial.println("sendNewDynCode");
-  int randNumber4digits = random(0,9999);
+  Serial.println(F("sendNewDynCode"));
+  int randNumber4digits = random(1000,9999);
 
   dyn_code = String(randNumber4digits);
   return "dyn_code "+dyn_code;
@@ -213,7 +242,7 @@ String sendNewDynCode()
 
 String checkDynCode(String input)
 { 
-  Serial.println("checkDynCode");
+  Serial.println(F("checkDynCode"));
   int pos = input.indexOf(dyn_code);
   
   if (pos > 0) {
@@ -231,7 +260,7 @@ String checkDynCode(String input)
 
 void openDoorNow()
 {
-  Serial.println("openDoorNow");
+  Serial.println(F("openDoorNow"));
   digitalWrite(pinTuer, LOW);   // Relais AN
   // delay(1000);              // wait for a second
   relaisOffEventId = timer.after(3000, closeRalais); // call closeRalais with delay
@@ -241,7 +270,7 @@ void openDoorNow()
 
 void closeRalais()
 {
-  Serial.println("closeRalais");
+  Serial.println(F("closeRalais"));
   digitalWrite(pinTuer, HIGH);   // Tür-Relaus aus
   relaisOffEventId = -1;
   
@@ -253,7 +282,7 @@ void closeRalais()
 
 void switchToState(int newState)
 {
-  Serial.print("switchToState "); Serial.println(newState);
+  Serial.print(F("switchToState ")); Serial.println(newState);
   mystate = (serverstate) newState;
   
   if (resetStateEventId >= 0) {
@@ -270,17 +299,18 @@ void switchToState(int newState)
 
 void resetState()
 {
-  Serial.println("resetState");
+  Serial.println(F("resetState"));
   switchToState(awaiting_fixed_pin);
   resetStateEventId = -1;
 }
 
+
 void sendEMail()
 {
-  Serial.println("sendEMail");
+  Serial.println(F("sendEMail"));
   EthernetClient client = EthernetClient();
   client.connect("mail.wor.net",25);
-  Serial.println("connected");
+  Serial.println(F("connected"));
   int count = 0;
   while (count < 20) {
     if (client.available()) {
@@ -289,27 +319,89 @@ void sendEMail()
         break;
     }
     else {
-      Serial.println("delay");
+      Serial.println(F("delay"));
       delay(1000);
     }
   }
-  Serial.println("got response from mail server");
-  client.print("HELO arduino.steinkopf.net\n");
+  Serial.println(F("got response from mail server"));
+  client.print(F("HELO arduino.steinkopf.net\n"));
   delay(100);
-  // client.print("AUTH PLAIN\n");
-  // client.print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\r\n");  // x: base64 encoded login: \0foo@yahoo.com\0password
-  client.print("MAIL FROM:<tueroeffner_arduino@steinkopf.net>\n");
-  client.print("RCPT TO:<dirk@wor.net>\n");
-  client.print("DATA\n");
+  // client.print(F("AUTH PLAIN\n"));
+  // client.print(F("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\r\n"));  // x: base64 encoded login: \0foo@yahoo.com\0password
+  client.print(F("MAIL FROM:<tueroeffner_arduino@steinkopf.net>\n"));
+  client.print(F("RCPT TO:<dirk@wor.net>\n"));
+  client.print(F("DATA\n"));
   delay(100);
-  client.print("From: \"tueroeffner_arduino\" <tueroeffner_arduino@steinkopf.net>\r\n");
-  client.print("To: dirk@wor.net\r\n");
-  client.print("Subject: Tuer geoeffnet\r\n");  // xD
-  client.print("\r\n");
-  client.print("Tuer wurde geoeffnet\r\n");  // Lines of text
+  client.print(F("From: \"tueroeffner_arduino\" <tueroeffner_arduino@steinkopf.net>\r\n"));
+  client.print(F("To: dirk@wor.net\r\n"));
+  client.print(F("Subject: Tuer geoeffnet\r\n"));  // xD
+  client.print(F("\r\n"));
+  client.print(F("Tuer wurde geoeffnet\r\n"));  // Lines of text
   // ......................................
-  client.print(".\r\n");
-  client.print("quit\n");
+  client.print(F(".\r\n"));
+  client.print(F("quit\n"));
   client.stop();
-  Serial.println("mail sent");
+  Serial.println(F("mail sent"));
+}
+
+int freeRam () {
+  extern int __heap_start, *__brkval; 
+  int v; 
+  return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval); 
+}
+
+void dht22()
+{ 
+  DHT22_ERROR_t fehlerCodeDHT22;
+  Serial.print(F("Hole Daten DHT22 ...: "));
+  fehlerCodeDHT22 = DHT22_1.readData(); // hole Status
+  switch(fehlerCodeDHT22) // werte Status aus
+  {
+    case DHT_ERROR_NONE:; // wenn kein Fehler
+      temperaturDHT22 = DHT22_1.getTemperatureC();
+      Serial.print(temperaturDHT22);
+      Serial.print(F(" C "));
+      feuchtigkeit = DHT22_1.getHumidity();
+      Serial.print(feuchtigkeit);
+      Serial.println(F(" %"));
+      break;
+    case DHT_ERROR_CHECKSUM:
+      Serial.print(F("Fehler Pruefziffer "));
+      break;
+    case DHT_BUS_HUNG:
+      Serial.println(F("BUS haengt "));
+      break;
+    case DHT_ERROR_NOT_PRESENT:
+      Serial.println(F("nicht vorhanden "));
+      break;
+    case DHT_ERROR_ACK_TOO_LONG:
+      Serial.println(F("Fehler ACK Timeout "));
+      break;
+    case DHT_ERROR_SYNC_TIMEOUT:
+      Serial.println(F("Fehler Sync Timeout "));
+      break;
+    case DHT_ERROR_DATA_TIMEOUT:
+      Serial.println(F("Fehler Daten Timeout "));
+      break;
+    case DHT_ERROR_TOOQUICK:
+      Serial.println(F("Abfrage zu schnell "));
+      break;
+  }  
+  Serial.println(freeRam());
+}
+
+String getTemperature()
+{
+  static char tempStr[10];
+  dtostrf(temperaturDHT22,1,1,tempStr);
+
+  return tempStr;
+}
+
+String getHumidity()
+{
+  static char tempStr[10];
+  dtostrf(feuchtigkeit,1,0,tempStr);
+
+  return tempStr;
 }
