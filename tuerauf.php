c947d555b6a53152987a12b6aed42848d5f09d28
Dirk Steinkopf <dirk@steinkopf.net>
2015-01-24 10:42:38 +0100

    Entfernung jetzt in Meter berechnen

--- tuerauf.php
+++ tuerauf.php
@@ -20,23 +20,16 @@ $arduino_baseurl = "http://arduino.steinkopf.net:1080/";
 
 $stkhomey = 48.109535;
 $stkhomex = 11.622306;
-$maxy = 48.109870;
-$maxx = 11.622764;
-$maxdiffy = $maxy - $stkhomey;
-$maxdiffx = $maxx - $stkhomex;
-$diffy = abs($geoy - $stkhomey);
-$diffx = abs($geox - $stkhomex);
+$maxdist = 80; // meter
+$dist = distance($stkhomey, $stkhomex, $geoy, $geox, "K") * 1000;
 
 if ($debug) {
     print "geoy = $geoy ";
     print "geox = $geox<br>\n";
-    print "maxdiffy = $maxdiffy ";
-    print "maxdiffx = $maxdiffx<br>\n";
-    print "diffy = $diffy ";
-    print "diffx = $diffx<br>\n";
+    print "dist = $dist meter<br>\n";
 }
 
-if ($diffx > $maxdiffx || $diffy > $maxdiffy) {
+if ($dist > $maxdist) {
     reject();
 }
 
@@ -76,4 +69,54 @@ function startsWith($haystack, $needle) {
     // search backwards starting from haystack length characters from the end
     return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== FALSE;
 }
+
+
+/*::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+/*::                                                                         :*/
+/*::  This routine calculates the distance between two points (given the     :*/
+/*::  latitude/longitude of those points). It is being used to calculate     :*/
+/*::  the distance between two locations using GeoDataSource(TM) Products    :*/
+/*::                     													 :*/
+/*::  Definitions:                                                           :*/
+/*::    South latitudes are negative, east longitudes are positive           :*/
+/*::                                                                         :*/
+/*::  Passed to function:                                                    :*/
+/*::    lat1, lon1 = Latitude and Longitude of point 1 (in decimal degrees)  :*/
+/*::    lat2, lon2 = Latitude and Longitude of point 2 (in decimal degrees)  :*/
+/*::    unit = the unit you desire for results                               :*/
+/*::           where: 'M' is statute miles                                   :*/
+/*::                  'K' is kilometers (default)                            :*/
+/*::                  'N' is nautical miles                                  :*/
+/*::  Worldwide cities and other features databases with latitude longitude  :*/
+/*::  are available at http://www.geodatasource.com                          :*/
+/*::                                                                         :*/
+/*::  For enquiries, please contact sales@geodatasource.com                  :*/
+/*::                                                                         :*/
+/*::  Official Web site: http://www.geodatasource.com                        :*/
+/*::                                                                         :*/
+/*::         GeoDataSource.com (C) All Rights Reserved 2015		     :*/
+/*::                                                                         :*/
+/*::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+function distance($lat1, $lon1, $lat2, $lon2, $unit) {
+
+  $theta = $lon1 - $lon2;
+  $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+  $dist = acos($dist);
+  $dist = rad2deg($dist);
+  $miles = $dist * 60 * 1.1515;
+  $unit = strtoupper($unit);
+
+  if ($unit == "K") {
+    return ($miles * 1.609344);
+  } else if ($unit == "N") {
+      return ($miles * 0.8684);
+    } else {
+        return $miles;
+      }
+}
+
+//echo distance(32.9697, -96.80322, 29.46786, -98.53506, "M") . " Miles<br>";
+//echo distance(32.9697, -96.80322, 29.46786, -98.53506, "K") . " Kilometers<br>";
+//echo distance(32.9697, -96.80322, 29.46786, -98.53506, "N") . " Nautical Miles<br>";
+
 ?>
