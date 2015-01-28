edbaff8ab2073e6994049221a48a3cea8fb2eebd
Dirk Steinkopf <dirk@steinkopf.net>
2015-01-28 19:55:46 +0100

    erste Prof-Daten

--- php/data/userlistall.php
+++ php/data/userlistall.php
@@ -1,3 +1,3 @@
 <?php
-$users="{\"54c540d25d6484.27231236\":\"Dirk\"}";
+$users="{\"03C085B2-D6C2-4172-A051-6BDB6ED93C12\":{\"name\":\"Simulator test\"},\"9A23FAAC-CF7E-44DA-9391-A9E18C42F144\":{\"name\":\"Simulator prod\"},\"4792D700-7793-41C3-A2A0-8966C3544EA7\":{\"name\":\"Dirk iPhone 6\"}}";
 ?>
--- php/data/userlistnew.php
+++ php/data/userlistnew.php
@@ -1,3 +1,3 @@
 <?php
-$users="{\"BECB0D0A-25EF-4F07-B543-A4170A55B4C5\":{\"name\":\"simulator3\"}}";
+$users="[]";
 ?>
