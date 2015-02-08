8179a0073d1477ffcec8905fa86984e18ff378eb
Dirk Steinkopf <dirk@steinkopf.net>
2015-02-08 06:06:47 +0100

    erste Fassung für Apple-Testflight via eCube

--- php/data/userlist.php
+++ php/data/userlist.php
@@ -1,3 +1,3 @@
 <?php
-$users="a:1:{s:3:\"ii1\";O:4:\"User\":6:{s:8:\"username\";s:3:\"nn1\";s:3:\"pin\";s:4:\"1111\";s:5:\"lfdid\";i:0;s:3:\"new\";i:0;s:6:\"active\";i:1;s:14:\"installationid\";s:3:\"ii1\";}}";
+$users="a:2:{s:36:\"03C085B2-D6C2-4172-A051-6BDB6ED93C12\";O:4:\"User\":6:{s:8:\"username\";s:10:\"simul test\";s:3:\"pin\";s:4:\"1234\";s:5:\"lfdid\";i:0;s:3:\"new\";i:0;s:6:\"active\";i:1;s:14:\"installationid\";s:36:\"03C085B2-D6C2-4172-A051-6BDB6ED93C12\";}s:36:\"07BBB785-3031-43FB-BC38-02933473A877\";O:4:\"User\":6:{s:8:\"username\";s:13:\"Dirk iPhone 6\";s:3:\"pin\";s:4:\"1793\";s:5:\"lfdid\";i:1;s:3:\"new\";i:0;s:6:\"active\";i:1;s:14:\"installationid\";s:36:\"07BBB785-3031-43FB-BC38-02933473A877\";}}";
 ?>
--- php/data/userlistall.php
+++
@@ -1,3 +0,0 @@
-<?php
-$users="{\"03C085B2-D6C2-4172-A051-6BDB6ED93C12\":{\"name\":\"Simulator test\"},\"9A23FAAC-CF7E-44DA-9391-A9E18C42F144\":{\"name\":\"Simulator prod\"},\"4792D700-7793-41C3-A2A0-8966C3544EA7\":{\"name\":\"Dirk iPhone 6\"}}";
-?>
