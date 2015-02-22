bdf0969fd0e4ca8dc1b423fd615946c4b1afefa2
Dirk Steinkopf <dirk@steinkopf.net>
2015-02-22 05:45:35 +0100

    add 100 to revcount (to make number increase despite change from master to public)

--- getgitrevision.sh
+++ getgitrevision.sh
@@ -3,7 +3,7 @@ set -x
 
 cd "$PROJECT_DIR"
 REVCOUNT=$(git rev-list --count HEAD)
-let BUILD_VERSION=$REVCOUNT
+let BUILD_VERSION=$REVCOUNT+100
 GITHASH=$(git rev-list --max-count=1 --pretty=format:%h HEAD | tail -1)
 GITMODIFIED=$(git diff --quiet HEAD || echo " M")
 BUILD_VERSION_DETAILS=`echo "$BUILD_VERSION-$GITHASH$GITMODIFIED"`
