e676488d70a82e08cb100b83946f6a015873592d
Dirk Steinkopf <dirk@steinkopf.net>
2016-10-03 17:28:21 +0200

    Update getrevision.sh for widget

--- getgitrevision.sh
+++ getgitrevision.sh
@@ -9,10 +9,13 @@ GITMODIFIED=$(git diff --quiet HEAD || echo " M")
 BUILD_VERSION_DETAILS=`echo "$BUILD_VERSION-$GITHASH$GITMODIFIED"`
 
 cd "$BUILT_PRODUCTS_DIR/$PRODUCT_NAME.app"
-RELEASE_VERSION=$(/usr/libexec/PListBuddy -c "Print CFBundleShortVersionString" Info.plist)
+# RELEASE_VERSION=$(/usr/libexec/PListBuddy -c "Print CFBundleShortVersionString" Info.plist)
 /usr/libexec/PListBuddy -c "Set CFBundleVersion $BUILD_VERSION" Info.plist
+/usr/libexec/PListBuddy -c "Set CFBundleVersion $BUILD_VERSION" tawidget/Info.plist
 
 # CFBundleVersionDetails = our own key
 /usr/libexec/PListBuddy -c "Delete CFBundleVersionDetails" Info.plist || true
+/usr/libexec/PListBuddy -c "Delete CFBundleVersionDetails" tawidget/Info.plist || true
 /usr/libexec/PListBuddy -c "Add CFBundleVersionDetails string $BUILD_VERSION_DETAILS" Info.plist
+/usr/libexec/PListBuddy -c "Add CFBundleVersionDetails string $BUILD_VERSION_DETAILS" tawidget/Info.plist
 
