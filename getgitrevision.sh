de6cc30d1563263af8ee84cfa431f0e0e794564d
Dirk Steinkopf <dirk@steinkopf.net>
2016-10-04 08:48:47 +0200

    Patch to set correct CFBundleVersion in both widget an app.

--- getgitrevision.sh
+++ getgitrevision.sh
@@ -8,14 +8,12 @@ GITHASH=$(git rev-list --max-count=1 --pretty=format:%h HEAD | tail -1)
 GITMODIFIED=$(git diff --quiet HEAD || echo " M")
 BUILD_VERSION_DETAILS=`echo "$BUILD_VERSION-$GITHASH$GITMODIFIED"`
 
-cd "$BUILT_PRODUCTS_DIR/$PRODUCT_NAME.app"
-# RELEASE_VERSION=$(/usr/libexec/PListBuddy -c "Print CFBundleShortVersionString" Info.plist)
-/usr/libexec/PListBuddy -c "Set CFBundleVersion $BUILD_VERSION" Info.plist
-/usr/libexec/PListBuddy -c "Set CFBundleVersion $BUILD_VERSION" tawidget/Info.plist
+cd "$BUILT_PRODUCTS_DIR"
 
-# CFBundleVersionDetails = our own key
-/usr/libexec/PListBuddy -c "Delete CFBundleVersionDetails" Info.plist || true
-/usr/libexec/PListBuddy -c "Delete CFBundleVersionDetails" tawidget/Info.plist || true
-/usr/libexec/PListBuddy -c "Add CFBundleVersionDetails string $BUILD_VERSION_DETAILS" Info.plist
-/usr/libexec/PListBuddy -c "Add CFBundleVersionDetails string $BUILD_VERSION_DETAILS" tawidget/Info.plist
+# RELEASE_VERSION=$(/usr/libexec/PListBuddy -c "Print CFBundleShortVersionString" $INFOPLIST_PATH)
+/usr/libexec/PListBuddy -c "Set :CFBundleVersion $BUILD_VERSION" $INFOPLIST_PATH
+/usr/libexec/PListBuddy -c "Set :CFBundleVersion $BUILD_VERSION" $DWARF_DSYM_FILE_NAME/Contents/Info.plist
 
+# CFBundleVersionDetails = our own new key
+/usr/libexec/PListBuddy -c "Delete :CFBundleVersionDetails" $INFOPLIST_PATH || true
+/usr/libexec/PListBuddy -c "Add :CFBundleVersionDetails string $BUILD_VERSION_DETAILS" $INFOPLIST_PATH
